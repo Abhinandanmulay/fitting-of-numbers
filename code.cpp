b4938c5792b2ca95eb17015882aec3ac9d2f63df
Abhinandan Mulay <abhinandanmulay07@gmail.com>
2026-02-15 19:47:35 +0530

    Added function calculatevalues

--- code.cpp
+++ code.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 #include <vector>
 using namespace std;
 
@@ -8,8 +9,8 @@ public:
     int n;
     vector<int> x;
     vector<float> y;
-    double sumx=0, sumy=0, sumxy=0, sumx2=0;
-    double a, b;
+    double sumx=0, sumy=0, sumxy=0, sumx2=0, sumy2=0;
+    double a, b, r;
 
     void takeinput() {
         cout << "Enter no of points: ";
@@ -29,6 +30,19 @@ public:
         }
     }
 
+    void calculatevalues() {
+        for (int i=0; i<n; i++) {
+            sumx+=x[i];
+            sumy+=y[i];
+            sumxy+=x[i]*y[i];
+            sumx2+=x[i]*x[i];
+            sumy2+=y[i]*y[i];
+        }
+        b = (sumx*sumxy-sumy*sumx2)/(sumx*sumx-n*sumx2);
+        a = (sumy-b*n)/sumx;
+        r = (n*sumxy-sumx*sumy)/sqrt((n*sumx2-sumx*sumx)*(n*sumy2-sumy*sumy));
+    }
+
     void display() {
         cout << left << setw(10) << "x"
         << left << setw(10) << "y"
@@ -42,12 +56,22 @@ public:
             << left << setw(10) << x[i]*x[i] << endl;   
         }  
         cout << "----------------------------------" << endl;
+        cout << left << setw(10) << sumx
+             << left << setw(10) << sumy
+             << left << setw(10) << sumxy
+             << left << setw(10) << sumx2 << endl;
+        cout << "----------------------------------" << endl;
+        cout << "For y = ax+b: " << endl
+             << "a = " << a << endl
+             << "b = " << b << endl
+             << "r = " << r << endl;
     }
 };
 
 int main() {
     fittingOnStrightLine t;
     t.takeinput();
+    t.calculatevalues();
     t.display();
     return 0;
 }
